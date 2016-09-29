60ce0435e30777b517d545b6f781dbe79b457efd
Victoria Ward <vcward@hawaii.rr.com>
2016-09-28 16:18:01 -1000

    add click event to landing page tabs

--- src/app/category-tree/category-tree.component.ts
+++ src/app/category-tree/category-tree.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output } from '@angular/core';
+import { CHART_DIRECTIVES } from 'angular2-highcharts';
 
 import { UheroApiService } from '../uhero-api.service';
 
@@ -12,6 +13,13 @@ export class CategoryTreeComponent implements OnInit {
 
   private categories;
   private categoryTree;
+  options: Object;
+  private series;
+  private dates;
+  private observations;
+  private levelValues;
+  private percValues;
+
 
   constructor(private _uheroAPIService: UheroApiService) {
   }
@@ -38,4 +46,89 @@ export class CategoryTreeComponent implements OnInit {
             },
             error => console.log('Error fetching categories'));
   }
+
+  drawSeries(id: number) {
+    this._uheroAPIService.fetchSeries(id)
+      .subscribe(
+        (series) => {
+          var series = series;
+          console.log(series);
+        },
+        error => console.log('Error fetching series')
+      );
+
+    this._uheroAPIService.fetchObservations(id)
+      .subscribe(
+        (observations) => {
+          var observations = observations.transformationResults
+          var level = observations[0].observations;
+          var perc = observations[1].observations;
+          var levelValues = [];
+          var percValues = [];
+          var dates = [];
+
+          for(var i = 0; i < level.length; i++) {
+            levelValues.push(+level[i].value);
+            dates.push(level[i].date);
+            percValues.push(+perc[i].value);
+          }
+          this.levelValues = levelValues;
+
+          this.options = {
+            chart: {
+              height: 200,
+              width: 200
+            },
+            title: {
+              text: ''
+            },
+            legend: {
+              enabled: false
+            },
+            credits: {
+              enabled: false
+            },
+            xAxis: [{
+              categories: dates.reverse(),
+              labels: {
+                enabled: false
+              },
+              lineWidth: 0,
+              tickLength: 0,
+            }],
+            yAxis: [{
+              labels: {
+                enabled: false
+              },
+              title: {
+                text: ''
+              },
+              gridLineColor: 'transparent'
+            }, {
+              title: {
+                text: ''
+              },
+              labels: {
+                enabled: false
+              },
+              gridLineColor: 'transparent',
+              opposite: true
+            }],
+            series: [{
+              name: 'Rate of Change (%)',
+              type: 'column',
+              color: '#1D667F',
+              data: percValues.reverse()
+            }, {
+              name: 'Total Visitor Arrivals',
+              type: 'line',
+              yAxis: 1,
+              color: '#F6A01B',
+              data: levelValues.reverse(),
+            }]
+          };
+
+        },
+        error => console.log('Error fetching observations'));
+  };
 }
