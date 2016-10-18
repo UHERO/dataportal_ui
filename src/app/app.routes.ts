cf08f79b095cbde6b8e22ba475c5c4c91892d012
Victoria Ward <vcward@hawaii.rr.com>
2016-10-17 15:38:11 -1000

    use hash for routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -27,4 +27,4 @@ const routes: Routes = [
 
 ];
 
-export const routing = RouterModule.forRoot(routes);
+export const routing = RouterModule.forRoot(routes, {useHash: true});
--- src/app/uhero-api.service.spec.ts
+++ src/app/uhero-api.service.spec.ts
@@ -1,13 +1,13 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, getTestBed, async, inject, tick } from '@angular/core/testing';
+import { TestBed, fakeAsync, inject, tick } from '@angular/core/testing';
 import { Http, BaseRequestOptions, ConnectionBackend, Response, ResponseOptions } from '@angular/http';
 import { MockBackend, MockConnection } from '@angular/http/testing';
 
 import { UheroApiService } from './uhero-api.service';
 import { CategoryTree } from './category-tree';
 
-/* describe('Service: UheroApi', () => {
+describe('Service: UheroApi', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
@@ -23,55 +23,37 @@ import { CategoryTree } from './category-tree';
     });
   });
 
-  it('should ...', inject([UheroApiService], (service: UheroApiService) => {
+  /* it('should ...', inject([UheroApiService], (service: UheroApiService) => {
     expect(service).toBeTruthy();
-  }));
-}); */
+  }));*/
 
-describe('Service: UheroApi', () => {
-  let mockBackend: MockBackend;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      providers: [
-        {
-          provide: Http, useFactory: (backend: ConnectionBackend, defaultOptions: BaseRequestOptions) => {
-            return new Http(backend, defaultOptions);
-          }, deps: [MockBackend, BaseRequestOptions]
-        },
-        {provide: UheroApiService, useClass: UheroApiService},
-        {provide: MockBackend, useClass: BaseRequestOptions},
-        {provide: BaseRequestOptions, useClass: BaseRequestOptions}
-      ]
+  /* it('should retrieve all categories',
+  inject([UheroApiService, MockBackend], fakeAsync((uheroApiService: UheroApiService, mockBackend: MockBackend) => {
+    let res: Response;
+    mockBackend.connections.subscribe(c => {
+      expect(c.request.url).toBe('http://api.uhero.hawaii.edu/v1/category');
+      let response = new ResponseOptions({
+        body: [
+          {
+            children: {
+              id: 8,
+              name: 'Test Subcategory',
+              parentId: 4
+            },
+            id: 4,
+            name: 'Test Category'
+          }
+        ]
+      });
+
+      c.mockRespond(new Response(response));
     });
-    // TestBed.compileComponents();
-    mockBackend = getTestBed().get(MockBackend);
-  }));
-
-  // API Service Tests
-  /* it('should fetch categories', async(() => {
-    let uheroApiService: UheroApiService //= getTestBed().get(UheroApiService);
-    getTestBed().compileComponents().then(() => {
-      mockBackend.connections.subscribe(
-        (connection: MockConnection) => {
-          connection.mockRespond(new Response(
-            new ResponseOptions({
-              body: [
-                {
-                  id: 26,
-                  name: 'Test Category'
-                }
-              ]
-            })
-          ));
-        });
-
-        uheroApiService = getTestBed().get(UheroApiService);
-        expect(uheroApiService).toBeDefined();
-
-        uheroApiService.fetchCategories().subscribe((categories: CategoryTree) => {
-          expect(categories.length).toBeDefined();
-        })
+    uheroApiService.fetchCategories().subscribe((response) => {
+      res = response;
     });
-  })); */
+    tick();
+    expect(res).toBeTruthy();
+  }))); */
+
 });
