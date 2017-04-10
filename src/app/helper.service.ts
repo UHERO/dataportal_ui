8020c68a27ccaafaddf8063d50a8cfa8b5ae9b3e
Victoria Ward <vcward@hawaii.rr.com>
2017-04-10 11:37:30 -1000

    fix wrong sign for values

--- src/app/helper.service.ts
+++ src/app/helper.service.ts
@@ -293,7 +293,12 @@ export class HelperService {
     let intString = '' + int, i = intString.length;
     let r = '';
     while ((i -= 3) > signCheck) { r = ',' + intString.substr(i, 3) + r; }
-    return intString.substr(0, i + 3) + r + (decimalString ? '.' + decimalString : '');
+    let returnValue = intString.substr(0, i + 3) + r + (decimalString ? '.' + decimalString : '');
+    // If int == 0, converting int to string drops minus sign
+    if (int == 0 && num < 0) {
+      return '-' + returnValue;
+    }
+    return returnValue;
   }
 
   // Get a unique array of available regions for a category
