1206489bc55153d44836b1fd0826cce3dd8e0b81
Victoria Ward <vward@hawaii.edu>
2017-07-25 14:48:11 -1000

    remove unused variable

--- src/app/single-series/single-series.component.ts
+++ src/app/single-series/single-series.component.ts
@@ -57,7 +57,7 @@ export class SingleSeriesComponent implements OnInit, AfterViewInit {
   }
 
   // Redraw chart when selecting a new region or frequency
-  goToSeries(saPair: boolean, siblings: Array<any>, freq: string, geo: string, sa: boolean) {
+  goToSeries(siblings: Array<any>, freq: string, geo: string, sa: boolean) {
     this.seasonallyAdjusted = sa;
     this.noSelection = null;
     // Get array of siblings for selected geo and freq
