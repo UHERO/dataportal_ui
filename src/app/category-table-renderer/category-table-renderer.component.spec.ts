e020f241c49ca83c111ee0a5275401224a1745b0
Victoria Ward <vward@hawaii.edu>
2019-09-19 16:16:20 -1000

    remove outdated http module import in spec files

--- src/app/category-table-renderer/category-table-renderer.component.spec.ts
+++ src/app/category-table-renderer/category-table-renderer.component.spec.ts
@@ -1,6 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterLinkStubDirective, ActivatedRouteStub, ActivatedRoute } from '../../testing/router-stubs';
-import { XHRBackend, BaseRequestOptions, ConnectionBackend, Response, ResponseOptions } from '@angular/http';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
 import { CategoryTableRendererComponent } from './category-table-renderer.component';
