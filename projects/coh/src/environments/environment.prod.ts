b185e53401f1baa087cf04b6d0158f482e1bdebf
Victoria Ward <vward@hawaii.edu>
2020-06-18 16:30:50 -1000

    fix base url for data portal prod builds

--- projects/coh/src/environments/environment.prod.ts
+++ projects/coh/src/environments/environment.prod.ts
@@ -1,9 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api.uhero.hawaii.edu/v1',
-  portalUrl: {
-    uhero: 'https://data.uhero.hawaii.edu/#',
-    nta: 'https://data.uhero.hawaii.edu/nta/#',
-    coh: 'https://data.uhero.hawaii.edu/coh/#'
-  }
+  portalUrl: 'https://data.uhero.hawaii.edu/coh/#'
 };
--- projects/coh/src/environments/environment.staging.ts
+++ projects/coh/src/environments/environment.staging.ts
@@ -1,9 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://stage-api.uhero.hawaii.edu/v1',
-  portalUrl: {
-    uhero: 'https://stage-data.uhero.hawaii.edu/#',
-    nta: 'https://stage-data.uhero.hawaii.edu/nta/#',
-    coh: 'https://stage-data.uhero.hawaii.edu/coh/#'
-  }
+  portalUrl: 'https://stage-data.uhero.hawaii.edu/coh/#'
 };
