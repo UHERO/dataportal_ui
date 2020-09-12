fa2ffc375339228e9448fd37c9cb4f8188a400b6
Victoria Ward <vward@hawaii.edu>
2020-09-11 17:14:30 -1000

    edit environments

--- projects/ccom/src/environments/environment.prod.ts
+++ projects/ccom/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://api.uhero.hawaii.edu/v1',
+  portalUrl: 'https://data.uhero.hawaii.edu/#'
 };
--- projects/ccom/src/environments/environment.ts
+++ projects/ccom/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://api.uhero.hawaii.edu/v1',
+  portalUrl: 'http://localhost:4200/#'
 };
 
 /*
